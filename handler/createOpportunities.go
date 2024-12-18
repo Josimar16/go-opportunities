17e6e147833bea7d9b7ffd2ed5ab2084ff8170cb
Josimar16 <josimarjr479@gmail.com>
2024-12-18 10:24:55 -0300

    Initialize handler dependencies and implement opportunity creation
    Added logger and database initialization in the handler package to manage dependencies. Updated the CreateOpportunitiesHandler to validate input, map data to the Opportunity schema, and persist it in the database with error handling. Modified route initialization to ensure handler dependencies are set up before defining routes.

--- handler/createOpportunities.go
+++ handler/createOpportunities.go
@@ -1,12 +1,36 @@
 package handler
 
 import (
+	"github.com/Josimar16/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpportunitiesHandler(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Hello World",
-	})
+	request := CreateOpportunityRequest{}
+
+	c.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error validating request: %v", err.Error())
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opportunity).Error; err != nil {
+		logger.Errorf("Error creating opportunity: %v", err)
+		sendError(c, http.StatusInternalServerError, "Error creating opportunity on database")
+		return
+	}
+
+	sendSuccess(c, http.StatusCreated, "create-opportunity", opportunity)
 }
