8df662443c9077f84dbc3115e44159e3628cdf0b
Josimar16 <josimarjr479@gmail.com>
2024-12-21 16:49:03 -0300

    Refactor ListOpportunitiesHandler to fetch data from DB
    Updated the handler to retrieve opportunities from the database instead of returning a hardcoded message. Added error handling for database query failures and adjusted response structure accordingly.

--- handler/listOpportunities.go
+++ handler/listOpportunities.go
@@ -1,12 +1,18 @@
 package handler
 
 import (
+	"github.com/Josimar16/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ListOpportunitiesHandler(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Hello World",
-	})
+	opportunities := []schemas.Opportunity{}
+
+	if err := db.Find(&opportunities).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, "Error listing opportunities on database")
+		return
+	}
+
+	sendSuccess(c, http.StatusOK, "list-opportunities", opportunities)
 }
