0cedcf862bec67d2876ae4e50cf5a65446039416
Josimar16 <josimarjr479@gmail.com>
2024-12-21 16:48:54 -0300

    Update showOpportunity handler to fetch opportunity by ID
    Revised the handler to retrieve an opportunity from the database using the provided ID. Added validation for missing or invalid IDs and appropriate error responses for not found cases. This improves functionality from a static message to dynamic data retrieval.

--- handler/showOpportunity.go
+++ handler/showOpportunity.go
@@ -1,12 +1,26 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/Josimar16/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ShowOpportunitiesHandler(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Hello World",
-	})
+	id := c.Param("id")
+
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamRequired("id", "param").Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{}
+
+	if err := db.First(&opportunity, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("opportunity with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(c, http.StatusOK, "show-opportunity", opportunity)
 }
